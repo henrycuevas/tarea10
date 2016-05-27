872a64578329812db69e397fe77bb0f870c3fc5d
henrycuevas <enriquecuevas1989@gmail.com>
2016-05-27 20:54:51 +0200

    Test1

--- ExamenJunio2013Prob1/test/TablonDeAnunciosTest.java
+++ ExamenJunio2013Prob1/test/TablonDeAnunciosTest.java
@@ -1,13 +1,24 @@
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class TablonDeAnunciosTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	TablonDeAnuncios tablon;
+	@Before
+	public void creaTablonInicial() {
+		tablon=new TablonDeAnuncios();
+	}
+	@After
+	public void eliminaTablonInicial() {
+		tablon=null;
 	}
 
+	@Test
+	public void testInicial() {
+		assertEquals(tablon.anunciosPublicados(),1);
+	}
 }
