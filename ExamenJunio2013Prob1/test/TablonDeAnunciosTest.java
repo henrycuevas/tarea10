ddbff89d83822d8a0612ad0fd266c5e0dd35ea4d
henrycuevas <enriquecuevas1989@gmail.com>
2016-05-27 22:15:28 +0200

    Test5

--- ExamenJunio2013Prob1/test/TablonDeAnunciosTest.java
+++ ExamenJunio2013Prob1/test/TablonDeAnunciosTest.java
@@ -21,15 +21,14 @@ public class TablonDeAnunciosTest {
 		tablon=null;
 	}
 	@Test
-	public void testAnuncioNOEmpresaSinSaldo() {
-		IBaseDeDatosDePagos pagos=(IBaseDeDatosDePagos) mock(IBaseDeDatosDePagos.class);
-		IBaseDeDatosDeAnunciantes anunciantes=mock(IBaseDeDatosDeAnunciantes.class);
-		when(anunciantes.buscarAnunciante("Juan")).thenReturn(true);
-		when(pagos.anuncianteTieneSaldo("Juan")).thenReturn(true);
-		Anuncio noEmpresa=new Anuncio("Prueba","AnuncioNOEmpresa","Juan");
-		tablon.publicarAnuncio(noEmpresa, anunciantes,pagos);
-		pagos.anuncioPublicado("Juan");
-		assertEquals(tablon.anunciosPublicados(),2);
+	public void testBuscarPorTituloEmpresa() {
+		Anuncio empresa1=new Anuncio("Prueba1","AnuncioEmpresa1","LA EMPRESA");
+		tablon.publicarAnuncio(empresa1, null,null);
+		Anuncio empresa2=new Anuncio("Prueba2","AnuncioEmpresa2","LA EMPRESA");
+		tablon.publicarAnuncio(empresa2, null,null);
+		int publicados=tablon.anunciosPublicados();
+		tablon.buscarAnuncioPorTitulo("Prueba2");
+		assertEquals(tablon.anunciosPublicados(),publicados);
 		
 	}
 }
