cd48dbe750f4f2ebd977d0791ecb8743d0957dc3
henrycuevas <enriquecuevas1989@gmail.com>
2016-05-27 22:07:58 +0200

    Test4

--- ExamenJunio2013Prob1/test/TablonDeAnunciosTest.java
+++ ExamenJunio2013Prob1/test/TablonDeAnunciosTest.java
@@ -1,10 +1,12 @@
 import static org.junit.Assert.*;
 import junio2013.Anuncio;
 import junio2013.IBaseDeDatosDeAnunciantes;
+import junio2013.IBaseDeDatosDePagos;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.mockito.Mockito.*;
 
 public class TablonDeAnunciosTest {
@@ -18,25 +20,16 @@ public class TablonDeAnunciosTest {
 	public void eliminaTablonInicial() {
 		tablon=null;
 	}
-
 	@Test
-	public void testAnuncioNOEmpresaIncrementa() {
-		IBaseDeDatosDePagos pagos=mock(IBaseDeDatosDePagos.class);
+	public void testAnuncioNOEmpresaSinSaldo() {
+		IBaseDeDatosDePagos pagos=(IBaseDeDatosDePagos) mock(IBaseDeDatosDePagos.class);
 		IBaseDeDatosDeAnunciantes anunciantes=mock(IBaseDeDatosDeAnunciantes.class);
-		when(anunciantes.buscarAnunciante("Juan")).thenReturn(false);
+		when(anunciantes.buscarAnunciante("Juan")).thenReturn(true);
 		when(pagos.anuncianteTieneSaldo("Juan")).thenReturn(true);
-		int anuncios=tablon.anunciosPublicados();
 		Anuncio noEmpresa=new Anuncio("Prueba","AnuncioNOEmpresa","Juan");
 		tablon.publicarAnuncio(noEmpresa, anunciantes,pagos);
-		assertEquals(tablon.anunciosPublicados(),anuncios);
+		pagos.anuncioPublicado("Juan");
+		assertEquals(tablon.anunciosPublicados(),2);
 		
-	}
-	private IBaseDeDatosDeAnunciantes mock(Class<IBaseDeDatosDeAnunciantes> class1) {
-		
-		return null;
-	}
-	private Object when(boolean buscarAnunciante) {
-		
-		return buscarAnunciante;
 	}
 }
